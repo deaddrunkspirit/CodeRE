1b5faa66892089c89e24517064d967f1cb9a59a8
deaddrunkspirit <enmosolkov@edu.hse.ru>
2021-03-15 01:43:34 +0300

    m

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: AppComponent
+  // },
+  // {
+  //   path: 'api/snippet/:link',
+  //   component: AppComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,62 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CodemirrorModule } from '@ctrl/ngx-codemirror';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatOptionModule} from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CodeBlockComponent } from './codeblock/code-block.component';
+import { LanguageTypeComponent } from './footer/languagetype/language-type.component';
+import { ThemeComponent } from './footer/theme/theme.component';
+import { LinkTypeComponent } from './footer/linktype/link-type.component';
+import { CopyLinkComponent } from './footer/copylinkbutton/copy-link.component';
+import { CopyCodeComponent } from './footer/copycodebutton/copy-code.component';
+import { CodeBlockService } from './codeblock/code-block.service';
+import { HttpService } from './http.service';
+import { SnippetComponent } from './snippet/snippet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CodeBlockComponent,
+    LanguageTypeComponent,
+    ThemeComponent,
+    LinkTypeComponent,
+    CopyLinkComponent,
+    CopyCodeComponent,
+    SnippetComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    CodemirrorModule,
+    FormsModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatFormFieldModule,
+    NgxMatSelectSearchModule,
+    MatButtonModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [CodeBlockService, HttpService],
+  bootstrap: [AppComponent, CodeBlockComponent]
 })
 export class AppModule { }
