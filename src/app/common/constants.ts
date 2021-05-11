57a4af34b567a80a455544aeb303c7fdb78eb22b
deaddrunkspirit <enmosolkov@edu.hse.ru>
2021-05-12 00:16:12 +0300

    v1.0.2

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -1,7 +1,7 @@
 export class Constants {
   public static TITLE: string = 'Code.RE';
   public static BASE_URL: string = 'http://159.203.84.225:8080/';
-  public static URL: string = 'http://159.203.84.225:4200/'
+  public static URL: string = 'http://localhost:4200/'
   public static LONG_PATTERN: RegExp = /\/\w\w\w\w\w\w/;
   public static SHORT_PATTERN: RegExp = /\/\w\w\w/;
 }
